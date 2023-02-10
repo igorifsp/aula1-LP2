b90fe175939804903b484e0f14972c6eecfe58ff
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-02-09 22:19:06 -0300

    Aula 1

---
+++ example-app/resources/views/loja.blade.php
@@ -0,0 +1,8 @@
+<div style="margin-top: 15px; border: 1px solid blue">
+
+    <h3>{{ $nome_loja }}</h3>
+    <p>{{ $endereco }}</p>
+    <p>{{ $cidade }}</p>
+
+
+</div>
---
+++ example-app/resources/views/vendas.blade.php
@@ -0,0 +1,15 @@
+<h1>Essa é a tela de vendas!!!</h1>
+
+<p>Que legal!!!!!</p>
+<p>Este é o <b>Melhor</b>jeito de aprender</p>
+<hr>
+<p>Nosso lema:</p>
+<p>Reutilizar código e separação de responsabilidades</p>
+
+@include ('loja', 
+[
+    'nome_loja' => 'Loja Mairiporã',
+    'endereco' => 'Av..Slagado Filho',
+    'cidade' => 'Guarulhos - S.P.'
+]
+);
